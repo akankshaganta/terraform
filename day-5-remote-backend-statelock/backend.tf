844ad3e538ba388a7a3bed08b83fb19ff33854f9
akankshaganta <akankshaganta23@gmail.com>
2026-06-05 16:41:35 +0530

    dyanamodb statelock

--- day-5-remote-backend-statelock/backend.tf
+++ day-5-remote-backend-statelock/backend.tf
@@ -2,7 +2,9 @@ terraform {
   backend "s3" {
     bucket = "akankshaganta"
     key = "terraform/day-5/terraform.tfstate"
-    use_lockfile = true  #s3 native locking process to prevent concurrent state modifications
+    dynamodb_table = "terraform-locks"
+    encrypt = true
+    # use_lockfile = true  #s3 native locking process to prevent concurrent state modifications
     region = "us-east-1"
   }
 }
