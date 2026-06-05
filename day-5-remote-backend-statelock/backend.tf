9ac80fbc3de5a96dc7653e852771224aa82e1664
akankshaganta <akankshaganta23@gmail.com>
2026-06-05 16:19:06 +0530

    use_lockfile

--- day-5-remote-backend-statelock/backend.tf
+++ day-5-remote-backend-statelock/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket = "akankshaganta"
     key = "terraform/day-5/terraform.tfstate"
+    use_lockfile = true  #s3 native locking process to prevent concurrent state modifications
     region = "us-east-1"
   }
 }
