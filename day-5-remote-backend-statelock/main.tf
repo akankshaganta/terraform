b9af3eac58169a175f70b2c20d9c7079b8af2d90
root <root@ip-172-31-39-5.ec2.internal>
2026-06-05 09:01:02 +0000

    merge corrections

--- day-5-remote-backend-statelock/main.tf
+++ day-5-remote-backend-statelock/main.tf
@@ -5,13 +5,14 @@ resource "aws_vpc" "dev-vpc" {
   }
 }
 
-<<<<<<< Updated upstream
+
 resource "aws_subnet" "subnet-1" {
   vpc_id = "vpc-04c5167a57089b9bf"
   cidr_block = "10.0.0.0/26"
   tags = {
     Name = "subnt-1"
-=======
+  }
+}
 resource "aws_subnet" "subnet-2" { 
 
   vpc_id = "vpc-04c5167a57089b9bf"
@@ -22,7 +23,7 @@ resource "aws_subnet" "subnet-2" {
 
     Name = "subnt-2" 
 
->>>>>>> Stashed changes
+
   }
 }
 
