c944e2153065cd7ea1940159c2637e86602c3845
akankshaganta <akankshaganta23@gmail.com>
2026-06-05 14:17:22 +0530

    day5test

--- day-5-remote-backend-statelock/main.tf
+++ day-5-remote-backend-statelock/main.tf
@@ -5,6 +5,22 @@ resource "aws_vpc" "dev-vpc" {
   }
 }
 
+resource "aws_subnet" "subnet-1" {
+  vpc_id = "vpc-04c5167a57089b9bf"
+  cidr_block = "10.0.0.0/26"
+  tags = {
+    Name = "subnt-1"
+  }
+}
+
+resource "aws_subnet" "subnet-3" {
+  vpc_id = "vpc-04c5167a57089b9bf"
+  cidr_block = "10.0.0.192/26"
+  tags = {
+    Name = "subnet-3"
+  }
+}
+
 provider "aws" {
   
 }
