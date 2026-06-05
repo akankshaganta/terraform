4481aa5977721da02e899dd3e0ad5c8d07336197
akankshaganta <akankshaganta23@gmail.com>
2026-06-05 16:12:30 +0530

    subnets recreation with new vpc

--- day-5-remote-backend-statelock/main.tf
+++ day-5-remote-backend-statelock/main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "dev-vpc" {
 
 
 resource "aws_subnet" "subnet-1" {
-  vpc_id = "vpc-04c5167a57089b9bf"
+  vpc_id = "vpc-0e4f32f71ed666b36"
   cidr_block = "10.0.0.0/26"
   tags = {
     Name = "subnt-1"
@@ -15,7 +15,7 @@ resource "aws_subnet" "subnet-1" {
 }
 resource "aws_subnet" "subnet-2" { 
 
-  vpc_id = "vpc-04c5167a57089b9bf"
+  vpc_id = "vpc-0e4f32f71ed666b36"
 
   cidr_block = "10.0.0.128/26" 
 
@@ -28,7 +28,7 @@ resource "aws_subnet" "subnet-2" {
 }
 
 resource "aws_subnet" "subnet-3" {
-  vpc_id = "vpc-04c5167a57089b9bf"
+  vpc_id = "vpc-0e4f32f71ed666b36"
   cidr_block = "10.0.0.192/26"
   tags = {
     Name = "subnet-3"
