bd59bfb16510b741d4844d3703a102f5fce6d60d
akankshaganta <akankshaganta23@gmail.com>
2026-06-06 16:34:14 +0530

    vpc,subnet-1

--- drift-issue-resolve/main.tf
+++ drift-issue-resolve/main.tf
@@ -5,6 +5,14 @@ resource "aws_vpc" "dev-vpc" {
   }
 }
 
+resource "aws_subnet" "subnet-1" {
+  vpc_id = "vpc-07116667b5f83079e"
+  cidr_block = "10.0.0.0/26"
+  tags = {
+    Name = "subnet-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
