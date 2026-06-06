e6a324d6ab435d1ba79f78e22733c1c85f9c0494
root <root@ip-172-31-40-98.ec2.internal>
2026-06-06 11:06:41 +0000

    vpc,subnet-2

--- drift-issue-resolve/main.tf
+++ drift-issue-resolve/main.tf
@@ -5,11 +5,19 @@ resource "aws_vpc" "dev-vpc" {
   }
 }
 
+<<<<<<< Updated upstream
 resource "aws_subnet" "subnet-1" {
   vpc_id = "vpc-07116667b5f83079e"
   cidr_block = "10.0.0.0/26"
   tags = {
     Name = "subnet-1"
+=======
+resource "aws_subnet" "subnet-2" {
+  vpc_id = "vpc-07116667b5f83079e"
+  cidr_block = "10.0.0.64/26"
+  tags = {
+    Name = "subnet-2"
+>>>>>>> Stashed changes
   }
 }
 
